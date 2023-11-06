940b0b984856df498d3cfad80002f9abc3224ae8
Aldyo Putra <106857082+dyooo123@users.noreply.github.com>
2023-11-06 11:30:52 +0700

    add : attendance type

--- app/Http/Controllers/Admin/AttendanceTypeController.php
+++ app/Http/Controllers/Admin/AttendanceTypeController.php
@@ -48,7 +48,36 @@ class AttendanceTypeController extends Controller
             $this->attendanceType->store($request->all());
             return redirect()->route('admin.attendance-type.index')->with('success', 'Jenis Kehadiran berhasil ditambahkan');
         } catch (\Exception $e) {
+            dd($e->getMessage());
             return redirect()->route('admin.attendance-type.index')->with('error', 'Jenis Kehadiran gagal ditambahkan');
         }
+    }
+
+    public function edit($id, Request $request)
+    {
+        return view('admin.attendance_type.edit', [
+            'attendanceType' => $this->attendanceType->getById($id)
+        ]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'name'     => 'required|unique:attendance_types,name,' . $id,
+        ]);
+
+        try {
+            $this->attendanceType->update($id, $request->all());
+            return redirect()->route('admin.attendance-type.index')->with('success', 'Tipe tugas berhasil diubah');
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+            return redirect()->route('admin.attendance-type.index')->with('error', 'Tipe tugas gagal diubah');
+        }
+    }
+
+    public function destroy($id)
+    {
+        $this->attendanceType->destroy($id);
+        return response()->json(true);
     }
 }
