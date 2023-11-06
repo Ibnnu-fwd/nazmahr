bbae2b00451985fb8b188681942a34a747d0233b
Iqbalrez <iqbalrr02@gmail.com>
2023-11-06 14:59:02 +0700

    add: date filter to attendance table

--- resources/views/admin/attendance/index.blade.php
+++ resources/views/admin/attendance/index.blade.php
@@ -4,6 +4,12 @@
     <x-breadcrumb name="admin.attendance" />
 
     <x-card-container>
+        <div class="flex items-center mb-4">
+            <label for="date" class="mr-2 block text-sm font-medium text-gray-700 ">Tanggal:</label>
+            <input type="date" id="date" name="date" value="{{ date('Y-m-d') }}"
+                class="border-gray-300 focus:border-yellow-500 text-sm text-gray-500 focus:ring-yellow-500 rounded-md shadow-sm block w-fit">
+        </div>
+
         <table class="rowTable">
             <thead>
                 <tr>
@@ -26,14 +32,19 @@
 
     @push('js-internal')
         <script>
-            $(function() {
+            $(document).ready(function() {
 
-                $('.rowTable').DataTable({
+                table = $('.rowTable').DataTable({
                     processing: true,
                     serverSide: true,
                     autoWidth: false,
                     responsive: true,
-                    ajax: "{{ route('admin.attendance.index') }}",
+                    ajax: {
+                        url: "{{ route('admin.attendance.index') }}",
+                        data: function(d) {
+                            d.date = $('#date').val();
+                        }
+                    },
                     columns: [{
                             data: 'DT_RowIndex',
                             name: 'DT_RowIndex'
@@ -75,6 +86,12 @@
                             name: 'overtime'
                         },
                     ]
+                });
+
+                $('#date').on('change', function(e) {
+                    var date = $(this).val();
+                    $('#date').val(date)
+                    table.column(2).search(date.split('-').reverse().join('-')).draw();
                 });
             });
 
