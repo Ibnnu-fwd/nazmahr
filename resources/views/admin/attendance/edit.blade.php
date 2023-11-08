25f67e863f1bb0d5fa8aba4ec98f6553cd317bf3
Iqbalrez <iqbalrr02@gmail.com>
2023-11-08 10:18:57 +0700

    fix: fix attendance edit option

--- resources/views/admin/attendance/edit.blade.php
+++ resources/views/admin/attendance/edit.blade.php
@@ -9,7 +9,8 @@
             @method('PUT')
             <x-select required id="user_id" name="user_id" label="{{ __('Nama Karyawan') }}">
                 @foreach ($employees as $employee)
-                    <option value="{{ $employee->id }}">{{ $employee->name }} </option>
+                    <option value="{{ $employee->id }}"
+                        @if ($employee->id == $attendance->user_id) selected>{{ $employee->name }} @endif</option>
                 @endforeach
             </x-select>
             <x-input :value="$attendance->entry_at" required id="entry_at" label="{{ __('Jam Masuk') }}" type="datetime-local"
@@ -18,9 +19,9 @@
                 name="exit_at" />
             <x-textarea id="description" label="{{ __('Keterangan') }}" name="description" :value="$attendance->description" required />
             <x-select id="status" name="status" label="{{ __('Status') }}" required>
-                <option value="0">Menunggu</option>
-                <option value="1">Disetujui</option>
-                <option value="2">Ditolak</option>
+                <option value="0" @if ($attendance->status == 0) selected @endif>Menunggu</option>
+                <option value="1" @if ($attendance->status == 1) selected @endif>Disetujui</option>
+                <option value="2" @if ($attendance->status == 2) selected @endif>Ditolak</option>
             </x-select>
             <x-button id="store" label="{{ __('Simpan Perubahan') }}" type="submit" />
         </form>
